76750ede903a20c2c4bad4c04112a44de98524cb
Cristian Martinez <cristianandres92@gmail.com>
2024-04-13 15:20:52 -0500

    Using take while, drop while, limit and skip stream operations

--- src/main/java/chapters/five/StreamSlicing.java
+++ src/main/java/chapters/five/StreamSlicing.java
@@ -3,6 +3,7 @@ package chapters.five;
 import java.util.Arrays;
 import java.util.List;
 
+import static chapters.five.Menu.menu;
 import static java.util.stream.Collectors.toList;
 
 public class StreamSlicing {
@@ -17,8 +18,6 @@ public class StreamSlicing {
         );
 
         // Slicing using a predicate
-
-        // Using takeWhile
         System.out.println("\nUsing the filter operation\n");
         List<Dish> filteredMenu = specialMenu.stream()
                 .filter(dish -> dish.calories() < 320)
@@ -26,6 +25,7 @@ public class StreamSlicing {
 
         System.out.println(filteredMenu);
 
+        // Using takeWhile
         System.out.println("\nUsing the takeWhile operation\n");
         List<Dish> slicedMenu1 = specialMenu.stream()
                 .takeWhile(dish -> dish.calories() < 320)
@@ -40,5 +40,34 @@ public class StreamSlicing {
                 .collect(toList());
 
         System.out.println(slicedMenu2);
+
+        // Truncating a stream
+        System.out.println("\nUsing the limit operation\n");
+        List<Dish> dishesLimit = specialMenu
+                .stream()
+                .filter(dish -> dish.calories() > 300)
+                .limit(3)
+                .collect(toList());
+
+        System.out.println(dishesLimit);
+
+        // Skipping elements
+        System.out.println("\nUsing the skip operation\n");
+        List<Dish> dishesSkip = menu().stream()
+                .filter(dish -> dish.calories() > 300)
+                .skip(2)
+                .collect(toList());
+
+        System.out.println(dishesSkip);
+
+        // Quiz filtering
+        // How would you use streams to filter the first two meat dishes?
+        System.out.println("\nQuiz filtering\n");
+        List<Dish> dishesLimitMeat = menu().stream()
+                .filter(dish -> dish.type() == Dish.Type.MEAT)
+                .limit(2)
+                .collect(toList());
+
+        System.out.println(dishesLimitMeat);
     }
 }
